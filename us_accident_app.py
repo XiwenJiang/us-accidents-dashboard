8b59ea23c0067a9e7e2ecfdc74da8e1704ae4374
XiwenJiang <xiwenj@uci.edu>
2025-01-22 14:48:58 -0800

    create the streamlit panel

---
+++ us_accident_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+from PIL import Image
+
+st.write("""
+         # US Accident Severity Prediction App
+         This app predicts the severity of a car accident in the US.
+         """)
