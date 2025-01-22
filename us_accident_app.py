f711b8a0d1182ea98ee37a2f22512ae7eb9a3c22
XiwenJiang <xiwenj@uci.edu>
2025-01-22 15:39:25 -0800

    for the introduction page, adding load data

--- us_accident_app.py
+++ us_accident_app.py
@@ -5,6 +5,16 @@ import pickle
 from PIL import Image
 
 st.write("""
-         # US Accident Severity Prediction App
-         This app predicts the severity of a car accident in the US.
+         # US Car Accidents App
+         The primary objectives of this analysis are to:
+            - **Visualize Geographic Data**: Leverage geospatial data to create intuitive and informative visualizations.
+            - **Identify Accident Hotspots**: Locate and analyze regions with high frequencies of traffic accidents.
          """)
+
+# Load dataset
+@st.cache_data
+def load_data():
+    data = pd.read_csv('temp/US_Accidents_March23_sampled_500k.csv')
+    return data
+
+data = load_data()
